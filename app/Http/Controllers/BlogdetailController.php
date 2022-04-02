7b0439cd645c65ced1e80245d70c542d08351c29
huyrobot <85402527+huyrobot@users.noreply.github.com>
2022-04-03 01:23:26 +0700

    UpdateURL_2.0

--- app/Http/Controllers/BlogdetailController.php
+++ app/Http/Controllers/BlogdetailController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BlogdetailController extends Controller
+class BlogDetailController extends Controller
 {
-    function blogdetail(){
+    function blogDetailFunction(){
         return view('blog-detail');
     }
 }
